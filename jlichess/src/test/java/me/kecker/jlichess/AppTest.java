f9e969cb8e1c869e1c59fe1ccc950a4fb9da0d2d
Lars Kecker <lars.kecker2009@gmail.com>
2021-04-11 18:49:57 +0200

    setup: maven project

---
+++ jlichess/src/test/java/me/kecker/jlichess/AppTest.java
@@ -0,0 +1,13 @@
+package me.kecker.jlichess;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
