0efa9fd772d53468fd282ba47dc790bbe77eeb7b
Lars Kecker <lars.kecker2009@gmail.com>
2021-04-12 22:59:20 +0200

    add(api): JLichess api wrapper class

---
+++ jlichess/src/main/java/me/kecker/jlichess/api/JLichess.java
@@ -0,0 +1,30 @@
+package me.kecker.jlichess.api;
+
+import java.io.IOException;
+import lombok.NonNull;
+import me.kecker.jlichess.model.Account;
+import me.kecker.jlichess.services.AccountRetrofitService;
+
+/**
+ * API class for the java lichess wrapper.
+ */
+public class JLichess {
+  private @NonNull String bearerToken;
+
+  private AccountService accountApi;
+
+  public JLichess(@NonNull String bearerToken) {
+    this.bearerToken = bearerToken;
+    this.setup();
+  }
+
+  public Account getAccountInfo() throws IOException {
+    return this.accountApi.getAccountInfo(this.bearerToken);
+  }
+
+  private void setup() {
+    this.accountApi = new AccountRetrofitService();
+  }
+
+
+}
