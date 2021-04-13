f4ff02d88fd1f53f2fb038fc038d6d02c1dfdc04
Lars Kecker <lars.kecker2009@gmail.com>
2021-04-13 18:36:43 +0200

    refactor(services): improve RetrofitFactory testability

--- jlichess/src/main/java/me/kecker/jlichess/services/AccountRetrofitService.java
+++ jlichess/src/main/java/me/kecker/jlichess/services/AccountRetrofitService.java
@@ -2,25 +2,31 @@ package me.kecker.jlichess.services;
 
 import java.io.IOException;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+
 import me.kecker.jlichess.api.AccountService;
+import me.kecker.jlichess.config.ApiUrl;
 import me.kecker.jlichess.http.clients.AccountHttpClient;
 import me.kecker.jlichess.model.Account;
 import me.kecker.jlichess.services.utils.RetrofitFactory;
 
 public class AccountRetrofitService implements AccountService {
 
-  private AccountHttpClient httpClient;
+    private AccountHttpClient httpClient;
 
-  public AccountRetrofitService() {
-    this.httpClient = RetrofitFactory.createHttpClient(AccountHttpClient.class);
-  }
+    public AccountRetrofitService() {
+        this(RetrofitFactory.getOrCreateRetrofit(ApiUrl.BASE_URL));
+    }
 
-  @Override
-  public Account getAccountInfo(String bearerToken) throws IOException {
+    public AccountRetrofitService(Retrofit retrofit) {
+        this.httpClient = retrofit.create(AccountHttpClient.class);
+    }
 
-    Response<Account> accountInfoResoponse = this.httpClient.getAccountInfo(bearerToken)
-      .execute();
-    return accountInfoResoponse.body();
-  }
-
+    @Override
+    public Account getAccountInfo(String bearerToken) throws IOException {
+        Response<Account> accountInfoResoponse = this.httpClient.getAccountInfo("Bearer "
+                + bearerToken)
+                .execute();
+        return accountInfoResoponse.body();
+    }
 }
--- jlichess/src/main/java/me/kecker/jlichess/services/utils/RetrofitFactory.java
+++ jlichess/src/main/java/me/kecker/jlichess/services/utils/RetrofitFactory.java
@@ -3,30 +3,40 @@ package me.kecker.jlichess.services.utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import me.kecker.jlichess.config.ApiUrl;
+
 import me.kecker.jlichess.http.adapters.LocalDateTimeTypeAdapter;
 
 public class RetrofitFactory {
 
-  private static Retrofit cache = null;
+    private static Map<HttpUrl, Retrofit> cache = new HashMap<>();
 
-  public static Retrofit getOrCreateRetrofit() {
-    if (cache != null) {
-      return cache;
+    public static Retrofit getOrCreateRetrofit(String baseUrl) {
+        return getOrCreateRetrofit(HttpUrl.get(baseUrl));
     }
-    Gson gson =
-        new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
-          .create();
-    Retrofit retrofit = new Retrofit.Builder().baseUrl(ApiUrl.BASE_URL)
-      .addConverterFactory(GsonConverterFactory.create(gson))
-      .build();
-    cache = retrofit;
-    return retrofit;
-  }
-  
-  public static <T> T createHttpClient(Class<T> clazz){
-    return getOrCreateRetrofit().create(clazz);
-  }
+
+    public static Retrofit getOrCreateRetrofit(HttpUrl baseUrl) {
+        if (cache.containsKey(baseUrl)) {
+            return cache.get(baseUrl);
+        }
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor)
+                .build();
+        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class,
+                new LocalDateTimeTypeAdapter())
+                .create();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        cache.put(baseUrl, retrofit);
+        return retrofit;
+    }
 }
