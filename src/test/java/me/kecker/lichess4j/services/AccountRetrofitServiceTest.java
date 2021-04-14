d86d9b7dfbf28b09699b3d4ae88199f8d65b8593
Lars Kecker <lars.kecker2009@gmail.com>
2021-04-14 20:58:37 +0200

    test(service): add AccountRetrofitServiceTest

--- src/test/java/me/kecker/lichess4j/services/AccountRetrofitServiceTest.java
+++ src/test/java/me/kecker/lichess4j/services/AccountRetrofitServiceTest.java
@@ -1,14 +1,16 @@
 package me.kecker.lichess4j.services;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import me.kecker.lichess4j.model.Account;
-import me.kecker.lichess4j.services.AccountRetrofitService;
 import me.kecker.lichess4j.services.utils.RetrofitFactory;
-import okhttp3.mockwebserver.MockResponse;
+import me.kecker.lichess4j.test.providers.AccountTestProvider;
+import me.kecker.lichess4j.test.utils.MockResponseUtils;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +18,7 @@ import retrofit2.Retrofit;
 
 public class AccountRetrofitServiceTest {
 
+    private static final String ACCOUNT_FILE_URL = "account.json";
     private static final String BEARER_TOKEN = "1234567890";
 
     private AccountRetrofitService objectUnderTest;
@@ -38,11 +41,22 @@ public class AccountRetrofitServiceTest {
     }
 
     @Test
-    public void getAccountInfo_happyDay_returnsAccountInfo() throws IOException {
-        this.mockWebServer.enqueue(new MockResponse().setBody("{\"id\": \"1234\"}"));
+    public void getAccountInfo_happyDay_returnsAccountInfo() throws IOException, InterruptedException {
+        this.mockWebServer.enqueue(MockResponseUtils.fromFile(ACCOUNT_FILE_URL,
+                AccountRetrofitServiceTest.class));
 
         Account result = this.objectUnderTest.getAccountInfo(BEARER_TOKEN);
+
+        // assert request is correct
+        RecordedRequest request = this.mockWebServer.takeRequest();
+        assertEquals("GET", request.getMethod());
+        assertEquals("/account", request.getPath());
+        assertEquals("Bearer " + BEARER_TOKEN, request.getHeader("Authorization"));
+        
+        // assert response is correct
         assertNotNull(result);
-        assertEquals("1234", result.getId());
+        assertThat(result).isEqualTo(AccountTestProvider.getAccount())
+                .usingRecursiveComparison();
+        
     }
 }
