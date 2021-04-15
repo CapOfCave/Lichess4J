f795e26d02712748577028521f8b5a026b6f4f85
Lars Kecker <lars.kecker2009@gmail.com>
2021-04-15 16:38:32 +0200

    add(http): Java 11 HttpClient

---
+++ src/main/java/me/kecker/lichess4j/http/base/HttpBaseClient.java
@@ -0,0 +1,48 @@
+package me.kecker.lichess4j.http.base;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import me.kecker.lichess4j.http.exceptions.IllegalStatusCodeException;
+import me.kecker.lichess4j.http.exceptions.UnauthorizedException;
+
+@RequiredArgsConstructor
+public class HttpBaseClient {
+
+    @NonNull
+    private String baseUrl;
+    @NonNull
+    private String bearerToken;
+    @NonNull
+    private Gson gson;
+
+    public <T> T get(String url, Class<T> entityClass) throws IOException, InterruptedException,
+            IllegalStatusCodeException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Authorization", "Bearer " + this.bearerToken)
+                .uri(URI.create(this.baseUrl + url))
+                .build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+        validateStatusCode(response.statusCode());
+
+        return gson.fromJson(response.body(), entityClass);
+
+    }
+
+    private void validateStatusCode(int statusCode) throws IllegalStatusCodeException {
+        if (statusCode == 401) {
+            throw new UnauthorizedException();
+        }
+        if (statusCode != 200) {
+            throw new IllegalStatusCodeException(statusCode);
+        }
+    }
+}
---
+++ src/main/java/me/kecker/lichess4j/http/utils/GsonFactory.java
@@ -0,0 +1,16 @@
+package me.kecker.lichess4j.http.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.time.LocalDateTime;
+import me.kecker.lichess4j.http.adapters.LocalDateTimeTypeAdapter;
+
+public class GsonFactory {
+
+    public static Gson getGson() {
+        return new GsonBuilder().registerTypeAdapter(LocalDateTime.class,
+                new LocalDateTimeTypeAdapter())
+                .create();
+    }
+
+}
