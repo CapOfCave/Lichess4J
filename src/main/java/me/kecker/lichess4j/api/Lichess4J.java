52ddb510732ccc5fc89ab5c0be57c4b25a22c29f
Lars Kecker <lars.kecker2009@gmail.com>
2021-04-15 20:19:29 +0200

    refactor(api)!: leak services instead of delegating each method

--- src/main/java/me/kecker/lichess4j/api/Lichess4J.java
+++ src/main/java/me/kecker/lichess4j/api/Lichess4J.java
@@ -1,13 +1,9 @@
 package me.kecker.lichess4j.api;
 
-import java.io.IOException;
 import lombok.NonNull;
 import me.kecker.lichess4j.config.ApiUrl;
 import me.kecker.lichess4j.http.base.HttpBaseClient;
-import me.kecker.lichess4j.http.exceptions.IllegalStatusCodeException;
-import me.kecker.lichess4j.http.exceptions.UnauthorizedException;
 import me.kecker.lichess4j.http.utils.GsonFactory;
-import me.kecker.lichess4j.model.Account;
 import me.kecker.lichess4j.services.AccountHttpService;
 
 /**
@@ -17,22 +13,22 @@ public class Lichess4J {
     private @NonNull String bearerToken;
 
     private HttpBaseClient httpBaseClient;
-    private AccountService accountApi;
+
+    private AccountService accountService;
 
     public Lichess4J(@NonNull String bearerToken) {
         this.bearerToken = bearerToken;
         this.setup();
     }
 
-    public Account getAccountInfo() throws IOException, InterruptedException,
-            UnauthorizedException, IllegalStatusCodeException {
-        return this.accountApi.getAccountInfo();
-    }
-
     private void setup() {
         this.httpBaseClient = new HttpBaseClient(ApiUrl.BASE_URL, this.bearerToken, GsonFactory
                 .getGson());
-        this.accountApi = new AccountHttpService(this.httpBaseClient);
+        this.accountService = new AccountHttpService(this.httpBaseClient);
+    }
+    
+    public AccountService account() {
+        return this.accountService;
     }
 
 }
