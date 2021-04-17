095829adc785bb75f740923aff48f6306dbf71de
Lars Kecker <lars.kecker2009@gmail.com>
2021-04-17 15:13:59 +0200

    test(services): add setKidModeStatus tests

--- src/test/java/me/kecker/lichess4j/services/AccountHttpServiceTest.java
+++ src/test/java/me/kecker/lichess4j/services/AccountHttpServiceTest.java
@@ -1,10 +1,17 @@
 package me.kecker.lichess4j.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.util.Map;
+import javax.lang.model.type.NullType;
 import me.kecker.lichess4j.http.base.HttpBaseClient;
 import me.kecker.lichess4j.http.exceptions.IllegalStatusCodeException;
 import me.kecker.lichess4j.model.account.Account;
@@ -20,6 +27,8 @@ import me.kecker.lichess4j.test.providers.PreferencesTestProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -36,6 +45,9 @@ public class AccountHttpServiceTest {
     @Mock
     private HttpBaseClient httpBaseClientMock;
 
+    @Captor
+    private ArgumentCaptor<BodyPublisher> bodyPublisherCaptor;
+
     @Before
     public void setup() throws IOException {
         this.objectUnderTest = new AccountHttpService(httpBaseClientMock);
@@ -45,8 +57,8 @@ public class AccountHttpServiceTest {
     public void getAccountInfo_happyDay_returnsAccountInfo() throws IOException,
             InterruptedException, IllegalStatusCodeException {
 
-        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, null, Account.class)).thenReturn(AccountTestProvider
-                .getAccount());
+        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, null, Account.class)).thenReturn(
+                AccountTestProvider.getAccount());
 
         Account result = this.objectUnderTest.getAccountInfo();
 
@@ -60,8 +72,8 @@ public class AccountHttpServiceTest {
     public void getEmail_happyDay_returnsEmail() throws IOException, InterruptedException,
             IllegalStatusCodeException {
 
-        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, EMAIL_URL, Email.class)).thenReturn(EmailTestProvider
-                .getEmail());
+        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, EMAIL_URL, Email.class)).thenReturn(
+                EmailTestProvider.getEmail());
 
         String result = this.objectUnderTest.getEmail();
 
@@ -74,8 +86,8 @@ public class AccountHttpServiceTest {
     public void getPreferences_happyDay_returnsPreferences() throws IOException,
             InterruptedException, IllegalStatusCodeException {
 
-        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, PREFERENCES_URL, PreferencesWrapper.class)).thenReturn(
-                PreferencesTestProvider.getPreferencesWrapper());
+        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, PREFERENCES_URL, PreferencesWrapper.class))
+                .thenReturn(PreferencesTestProvider.getPreferencesWrapper());
 
         Preferences result = this.objectUnderTest.getPreferenes();
 
@@ -88,13 +100,38 @@ public class AccountHttpServiceTest {
     public void getKidModeStatus_happyDay_returnKidModeStatus() throws IOException,
             InterruptedException, IllegalStatusCodeException {
 
-        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, KID_MODE_STATUS_URL, KidModeStatusWrapper.class)).thenReturn(
-                KidModeStatusTestProvider.getKidModeStatusWrapper());
+        when(httpBaseClientMock.get(ACCOUNT_BASE_URL, KID_MODE_STATUS_URL,
+                KidModeStatusWrapper.class)).thenReturn(KidModeStatusTestProvider
+                        .getKidModeStatusWrapper());
 
         KidModeStatus result = this.objectUnderTest.getKidModeStatus();
 
         assertNotNull(result);
         assertThat(result).isEqualTo(KidModeStatusTestProvider.getKidModeStatus());
 
+    }
+
+    @Test
+    public void setKidModeStatus_setToKid_setsKidModeStatus() throws IOException,
+            InterruptedException, IllegalStatusCodeException {
+
+        this.objectUnderTest.setKidModeStatus(KidModeStatus.KID);
+
+        verify(httpBaseClientMock, times(1)).post(eq(ACCOUNT_BASE_URL), eq(KID_MODE_STATUS_URL), eq(
+                Map.of("v", "true")), eq(NullType.class), this.bodyPublisherCaptor.capture());
+        assertEquals(0L, this.bodyPublisherCaptor.getValue()
+                .contentLength());
+    }
+
+    @Test
+    public void setKidModeStatus_setToNoKid_setsKidModeStatus() throws IOException,
+            InterruptedException, IllegalStatusCodeException {
+
+        this.objectUnderTest.setKidModeStatus(KidModeStatus.NO_KID);
+
+        verify(httpBaseClientMock, times(1)).post(eq(ACCOUNT_BASE_URL), eq(KID_MODE_STATUS_URL), eq(
+                Map.of("v", "false")), eq(NullType.class), this.bodyPublisherCaptor.capture());
+        assertEquals(0L, this.bodyPublisherCaptor.getValue()
+                .contentLength());
     }
 }
